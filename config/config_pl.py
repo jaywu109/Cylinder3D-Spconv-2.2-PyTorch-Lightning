ad372f5fbcf30965d92cb8b5e0c6ba976e8ab3da
Jay Wu <e14051350@gs.ncku.edu.tw>
2023-03-19 18:09:57 +0800

    first commit

---
+++ config/config_pl.py
@@ -0,0 +1,103 @@
+# -*- coding:utf-8 -*-
+# author: Xinge
+
+from pathlib import Path
+
+from strictyaml import Bool, Float, Int, Map, Seq, Str, as_document, load
+
+model_params = Map(
+    {
+        "model_architecture": Str(),
+        "output_shape": Seq(Int()),
+        "fea_dim": Int(),
+        "out_fea_dim": Int(),
+        "num_class": Int(),
+        "num_input_features": Int(),
+        "use_norm": Bool(),
+        "init_size": Int(),
+    }
+)
+
+dataset_params = Map(
+    {
+        "dataset_type": Str(),
+        "pc_dataset_type": Str(),
+        "ignore_label": Int(),
+        "return_test": Bool(),
+        "fixed_volume_space": Bool(),
+        "label_mapping": Str(),
+        "max_volume_space": Seq(Float()),
+        "min_volume_space": Seq(Float()),
+    }
+)
+
+
+train_data_loader = Map(
+    {
+        "data_path": Str(),
+        "imageset": Str(),
+        "return_ref": Bool(),
+        "batch_size": Int(),
+        "shuffle": Bool(),
+        "num_workers": Int(),
+    }
+)
+
+val_data_loader = Map(
+    {
+        "data_path": Str(),
+        "imageset": Str(),
+        "return_ref": Bool(),
+        "batch_size": Int(),
+        "shuffle": Bool(),
+        "num_workers": Int(),
+    }
+)
+
+train_params = Map(
+    {
+        "gpus": Seq(Int()),
+        "logdir": Str(),
+        "max_num_epochs": Int(),
+        "checkpoint_every_n_epochs": Float(),
+        "base_lr": Float(),
+        "weight_decay": Float(),
+        "mixed_fp16": Bool()
+     }
+)
+
+schema_v4 = Map(
+    {
+        "format_version": Int(),
+        "model_params": model_params,
+        "dataset_params": dataset_params,
+        "train_data_loader": train_data_loader,
+        "val_data_loader": val_data_loader,
+        "train_params": train_params,
+    }
+)
+
+
+SCHEMA_FORMAT_VERSION_TO_SCHEMA = {4: schema_v4}
+
+
+def load_config_data(path: str) -> dict:
+    yaml_string = Path(path).read_text()
+    cfg_without_schema = load(yaml_string, schema=None)
+    schema_version = int(cfg_without_schema["format_version"])
+    if schema_version not in SCHEMA_FORMAT_VERSION_TO_SCHEMA:
+        raise Exception(f"Unsupported schema format version: {schema_version}.")
+
+    strict_cfg = load(yaml_string, schema=SCHEMA_FORMAT_VERSION_TO_SCHEMA[schema_version])
+    cfg: dict = strict_cfg.data
+    return cfg
+
+
+def config_data_to_config(data):  # type: ignore
+    return as_document(data, schema_v4)
+
+
+def save_config_data(data: dict, path: str) -> None:
+    cfg_document = config_data_to_config(data)
+    with open(Path(path), "w") as f:
+        f.write(cfg_document.as_yaml())
---
+++ train_cylinder_asym_pl.py
@@ -0,0 +1,275 @@
+# -*- coding:utf-8 -*-
+# author: Xinge
+# @file: train_cylinder_asym.py
+
+import os
+import argparse
+import random
+import numpy as np
+import math
+import os
+import time
+import argparse
+import sys
+import torch
+# torch.set_float32_matmul_precision('high')
+
+import torch.optim as optim
+from tqdm import tqdm
+
+from utils.metric_util import per_class_iu, fast_hist_crop_torch
+from dataloader.pc_dataset import get_SemKITTI_label_name
+from dataloader.dataset_semantickitti import  collate_fn_BEV
+from builder import data_builder_pl, model_builder, loss_builder
+from config.config_pl import load_config_data
+from pytorch_lightning import loggers as pl_loggers
+from pytorch_lightning.callbacks import *
+import pytorch_lightning as pl
+from pytorch_lightning.strategies import DDPStrategy
+from multiprocessing import get_context
+
+from utils.load_save_util import load_checkpoint
+
+import warnings
+
+warnings.filterwarnings("ignore")
+
+class Cylinder3D(pl.LightningModule):
+    def __init__(self, configs):
+        super().__init__()
+
+        seed=123 
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        self.save_hyperparameters(configs)        
+
+        self.dataset_config = configs['dataset_params']
+        self.train_dataloader_config = configs['train_data_loader']
+        self.val_dataloader_config = configs['val_data_loader']
+        self.val_batch_size = self.val_dataloader_config['batch_size']
+        self.train_batch_size = self.train_dataloader_config['batch_size']
+        self.model_config = configs['model_params']
+        self.train_hypers = configs['train_params']
+        self.gpu_num = len(self.train_hypers['gpus'])
+        self.grid_size = self.model_config['output_shape']   
+
+        SemKITTI_label_name = get_SemKITTI_label_name(self.dataset_config["label_mapping"])
+        self.unique_label = np.asarray(sorted(list(SemKITTI_label_name.keys())))[1:] - 1
+        self.unique_label_str = [SemKITTI_label_name[x] for x in self.unique_label + 1]
+
+        self.model = model_builder.build(self.model_config)     
+
+        self.loss_func, self.lovasz_softmax = loss_builder.build(wce=True, lovasz=True,
+                                                    num_class=self.model_config['num_class'], ignore_label=self.dataset_config['ignore_label'])
+
+        if self.gpu_num > 1:
+            self.effective_lr = math.sqrt((self.gpu_num)*(self.train_batch_size)) * self.train_hypers["base_lr"]
+        else:
+            self.effective_lr = math.sqrt(self.train_batch_size) * self.train_hypers["base_lr"]
+
+        self.save_hyperparameters({'effective_lr': self.effective_lr})
+
+        self.train_dataset, self.val_dataset = data_builder_pl.build(self.dataset_config,
+                                                                        self.train_dataloader_config,
+                                                                        self.val_dataloader_config,
+                                                                        grid_size=self.grid_size) 
+        self.training_step_outputs = []
+        self.validation_step_outputs = []
+
+    def setup(self, stage=None):
+        """
+        make datasets & seeding each worker separately
+        """
+        ##############################################
+        # NEED TO SET THE SEED SEPARATELY HERE
+        seed = self.global_rank
+       
+        random.seed(seed)
+        np.random.seed(seed)
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        torch.cuda.manual_seed_all(seed)
+        print('local seed:', seed)
+        ##############################################      
+
+    def train_dataloader(self):          
+        return torch.utils.data.DataLoader(dataset=self.train_dataset,
+                                           batch_size=self.train_dataloader_config["batch_size"],
+                                           collate_fn=collate_fn_BEV,
+                                           shuffle=self.train_dataloader_config["shuffle"],
+                                           num_workers=self.train_dataloader_config["num_workers"],
+                                           pin_memory=True,
+                                           drop_last=True,
+                                           multiprocessing_context=get_context('fork')
+                                           )
+    
+    def val_dataloader(self):
+        return torch.utils.data.DataLoader(dataset=self.val_dataset,
+                                           batch_size=self.val_dataloader_config["batch_size"],
+                                           collate_fn=collate_fn_BEV,
+                                           shuffle=self.val_dataloader_config["shuffle"],
+                                           num_workers=self.val_dataloader_config["num_workers"],
+                                           pin_memory=True,
+                                           multiprocessing_context=get_context('fork')
+                                           )
+    
+    def forward(self, pt_fea_ten, vox_ten, batch_size):
+        outputs = self.model(pt_fea_ten, vox_ten, batch_size)
+        return outputs
+
+
+    def training_step(self, batch, batch_idx):
+        _, vox_label, grid, _, pt_fea = batch
+        pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(self.device) for i in
+                            pt_fea]
+        vox_ten = [torch.from_numpy(i).to(self.device) for i in grid]
+        label_tensor = vox_label.type(torch.LongTensor).to(self.device)   
+        batch_size = len(pt_fea)
+
+        predict_vox_labels = self(pt_fea_ten, vox_ten, batch_size)
+        loss = self.lovasz_softmax(torch.nn.functional.softmax(predict_vox_labels), label_tensor, ignore=0) + self.loss_func(
+            predict_vox_labels, label_tensor)              
+        
+        train_loss_gather = self.all_gather(loss).mean()
+
+        if self.global_rank == 0:
+            self.log('train_loss_step', train_loss_gather, prog_bar=True, rank_zero_only=True)
+            self.logger.experiment.add_scalar('train_loss_step', train_loss_gather, self.global_step)     
+
+        self.training_step_outputs.append({'loss': loss})
+        return loss
+
+    def on_training_epoch_end(self):
+        train_loss_mean = torch.stack([output['loss'] for output in self.training_step_outputs]).mean()   
+
+        if self.gpu_num > 1:     
+            self.log('train_loss_epoch', torch.mean(self.all_gather(train_loss_mean)), prog_bar=True, logger=True)
+        else:
+            self.log('train_loss_epoch', train_loss_mean, prog_bar=True, logger=True)
+
+        self.training_step_outputs.clear()
+
+    def validation_step(self, batch, batch_idx):
+        _, vox_label, grid, pt_labs, pt_fea = batch
+        pt_fea_ten = [torch.from_numpy(i).type(torch.FloatTensor).to(self.device) for i in
+                            pt_fea]
+        vox_ten = [torch.from_numpy(i).to(self.device) for i in grid]
+        label_tensor = vox_label.type(torch.LongTensor).to(self.device)   
+        batch_size = len(pt_fea)
+
+        predict_vox_labels = self(pt_fea_ten, vox_ten, batch_size)
+        loss = self.lovasz_softmax(torch.nn.functional.softmax(predict_vox_labels), label_tensor, ignore=0) + self.loss_func(
+            predict_vox_labels, label_tensor)    
+        
+        predict_vox_labels = torch.argmax(predict_vox_labels, dim=1)
+        hist_list = []
+        for count, _ in enumerate(grid):
+            hist_list.append(fast_hist_crop_torch(predict_vox_labels[
+                                                count, grid[count][:, 0], grid[count][:, 1],
+                                                grid[count][:, 2]], torch.tensor(pt_labs[count]).type(torch.LongTensor).to(self.device),
+                                            self.unique_label))
+
+        self.validation_step_outputs.append({'loss':loss, 'hist': sum(hist_list)})
+
+    def on_validation_epoch_end(self):
+        loss_list = []
+        hist_list = []
+        for output in self.validation_step_outputs:
+            loss_list.append(output['loss'])
+            hist_list.append(output['hist'])
+
+        val_loss_mean = torch.stack(loss_list).mean() 
+        hist_sum = sum(hist_list)
+
+        if self.gpu_num > 1:     
+            self.log('val_loss_epoch', torch.mean(self.all_gather(val_loss_mean)), prog_bar=True, logger=True)
+            iou = per_class_iu(sum(self.all_gather(hist_sum)).cpu().detach().numpy())
+        else:
+            self.log('val_loss_epoch', val_loss_mean, prog_bar=True, logger=True)
+            iou = per_class_iu(hist_sum.cpu().detach().numpy())
+
+        val_miou = np.nanmean(iou) * 100
+        self.log('val_miou_epoch', val_miou, prog_bar=True, logger=True)
+
+        self.validation_step_outputs.clear()
+
+    def configure_optimizers(self):
+        optimizer = torch.optim.AdamW(self.model.parameters(), lr=self.effective_lr, eps=1e-4, weight_decay=self.train_hypers['weight_decay'])
+        scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, self.trainer.max_epochs)
+        return {'optimizer':optimizer, 'lr_scheduler':scheduler}
+
+
+if __name__ == '__main__':
+    # Training settings
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-y', '--config_path', default='config/semantickitti_pl.yaml')
+    args = parser.parse_args()
+
+    config_path = args.config_path
+    configs = load_config_data(config_path)    
+    train_params = configs['train_params']
+    logdir = train_params['logdir']
+
+    model = Cylinder3D(configs)
+
+    gpus = train_params['gpus']
+    gpu_num = len(gpus)
+    print(gpus)
+
+    train_data_num = len(model.train_dataset)
+    effective_train_batch_size = configs['train_data_loader']['batch_size'] * gpu_num
+    train_steps_per_epoch = int(train_data_num // effective_train_batch_size)
+    val_interval_fraction = train_params['checkpoint_every_n_epochs']
+
+    if train_params['mixed_fp16']:
+        precision = '16'
+    else:
+        precision = '32'
+
+    if not os.path.exists(logdir):
+        os.makedirs(logdir)
+
+    checkpoint = ModelCheckpoint(dirpath=f'{logdir}',
+                                 save_last=True,
+                                 save_top_k=5,
+                                 mode='max',
+                                 every_n_train_steps=int(train_steps_per_epoch*val_interval_fraction)+1,
+                                #  every_n_epochs=1,
+                                 filename="{epoch:02d}-{step}-{val_miou_epoch:.3f}",
+                                 monitor="val_miou_epoch")
+    tb_logger = pl_loggers.TensorBoardLogger(save_dir=logdir,
+                                             name=None,
+                                             version='')
+
+    if gpu_num > 1:
+        strategy='ddp'
+        use_distributed_sampler = True
+    else:
+        strategy = 'auto'
+        use_distributed_sampler = False
+
+    # resume = '/public/home/wudj/Cylinder3D_uda/logs_2/epoch=09-step=9243-val_loss_epoch=1.7565454245.ckpt'
+    trainer = pl.Trainer(
+        # limit_val_batches=10,
+        # resume_from_checkpoint=resume,
+        val_check_interval=val_interval_fraction,
+        max_epochs=train_params['max_num_epochs'],
+        devices=gpus,
+        num_nodes=1,
+        precision = precision,
+        accelerator='gpu',
+        callbacks=[
+            checkpoint,
+            LearningRateMonitor(),
+        ],
+        strategy=strategy,
+        use_distributed_sampler=use_distributed_sampler,
+        logger=tb_logger,
+    )
+
+
+    trainer.fit(model)    
+
+
